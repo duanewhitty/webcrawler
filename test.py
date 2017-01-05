5e97843bce5e0634ff98bcad96b9cc75a2483d74
Duane <duane@nofroth.com>
2017-01-05 17:46:00 -0400

    Add first test of webcrawler library to repo

---
+++ test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import webcrawler
+
+
+
+index, graph = webcrawler.crawl_web('http://udacity.com/cs101x/urank/index.html')
+ranks = webcrawler.compute_ranks(graph)
+print ranks
+
+#>>> {'http://udacity.com/cs101x/urank/kathleen.html': 0.11661866666666663,
+#'http://udacity.com/cs101x/urank/zinc.html': 0.038666666666666655,
+#'http://udacity.com/cs101x/urank/hummus.html': 0.038666666666666655,
+#'http://udacity.com/cs101x/urank/arsenic.html': 0.054133333333333325,
+#'http://udacity.com/cs101x/urank/index.html': 0.033333333333333326,
+#'http://udacity.com/cs101x/urank/nickel.html': 0.09743999999999997}
+
+
+
